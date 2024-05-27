81e982acb5c1efe00dcdc151a3abb7d799bdfcf5
tomasdaureli <tomasdaureli@gmail.com>
2024-05-27 14:42:18 -0300

    add validation to auth controller

--- src/main/java/tpo/api/ecommerce/domain/AuthenticationRequestDTO.java
+++ src/main/java/tpo/api/ecommerce/domain/AuthenticationRequestDTO.java
@@ -2,6 +2,8 @@ package tpo.api.ecommerce.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,8 +16,10 @@ import lombok.Setter;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class AuthenticationRequestDTO {
 
+    @Email
     private String email;
 
+    @NotBlank
     private String password;
 
 }
--- src/main/java/tpo/api/ecommerce/domain/RegisterRequestDTO.java
+++ src/main/java/tpo/api/ecommerce/domain/RegisterRequestDTO.java
@@ -2,6 +2,8 @@ package tpo.api.ecommerce.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,12 +16,16 @@ import lombok.Setter;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class RegisterRequestDTO {
 
+    @NotBlank
     private String name;
 
+    @NotBlank
     private String lastName;
 
+    @Email
     private String email;
 
+    @NotBlank
     private String password;
 
 }
