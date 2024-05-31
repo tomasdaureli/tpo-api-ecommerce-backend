c35d64633282d8948fbc26a7eee69e1e4bf3a2cb
gregoriocarranza <gregoriocarranza@hotmail.com>
2024-05-31 19:27:31 -0300

    Roles in buys

--- src/main/java/tpo/api/ecommerce/controller/BuyController.java
+++ src/main/java/tpo/api/ecommerce/controller/BuyController.java
@@ -5,11 +5,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import tpo.api.ecommerce.domain.BuyDTO;
 import tpo.api.ecommerce.domain.CreateBuyRequestDTO;
-import tpo.api.ecommerce.domain.CreateBuyResponseDTO;
+import tpo.api.ecommerce.entity.UserRoles;
 import tpo.api.ecommerce.service.BuyService;
+import tpo.api.ecommerce.utils.UserValidations;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,29 +24,57 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class BuyController {
 
     private final BuyService service;
+    private final UserValidations userValidations;
 
     @PostMapping
-    public CreateBuyResponseDTO createBuy(
+    public ResponseEntity<?> createBuy(
             @Valid @RequestBody CreateBuyRequestDTO request) {
-        return service.createBuy(request);
+        try {
+            userValidations.validateRole(UserRoles.COMPRADOR);
+            return ResponseEntity.ok(service.createBuy(request));
+
+        } catch (IllegalStateException e) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
+        }
     }
 
     @GetMapping("/{buyNumber}")
-    public BuyDTO getBuyByNumber(
+    public ResponseEntity<?> getBuyByNumber(
             @PathVariable Long buyNumber) {
-        return service.getBuyByNumber(buyNumber);
+        try {
+            userValidations.validateRole(UserRoles.COMPRADOR);
+            return ResponseEntity.ok(service.getBuyByNumber(buyNumber));
+
+        } catch (IllegalStateException e) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
+        }
+
     }
 
     @PatchMapping("/{buyNumber}/confirm")
-    public BuyDTO confirmBuy(
+    public ResponseEntity<?> confirmBuy(
             @PathVariable Long buyNumber) {
-        return service.confirmBuy(buyNumber);
+        try {
+            userValidations.validateRole(UserRoles.COMPRADOR);
+            return ResponseEntity.ok(service.confirmBuy(buyNumber));
+
+        } catch (IllegalStateException e) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
+        }
+
     }
 
     @PatchMapping("/{buyNumber}/cancel")
-    public BuyDTO cancelBuy(
+    public ResponseEntity<?> cancelBuy(
             @PathVariable Long buyNumber) {
-        return service.cancelBuy(buyNumber);
+        try {
+            userValidations.validateRole(UserRoles.COMPRADOR);
+            return ResponseEntity.ok(service.cancelBuy(buyNumber));
+
+        } catch (IllegalStateException e) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
+        }
+
     }
 
 }
