06c3435349b7d216a3f6ffc894aa0ea8b03f8d1b
tomasdaureli <tomasdaureli@gmail.com>
2024-07-03 22:30:06 -0300

    delete cors annotation

--- src/main/java/tpo/api/ecommerce/controller/AuthController.java
+++ src/main/java/tpo/api/ecommerce/controller/AuthController.java
@@ -10,7 +10,6 @@ import tpo.api.ecommerce.domain.AuthenticationResponseDTO;
 import tpo.api.ecommerce.domain.RegisterRequestDTO;
 import tpo.api.ecommerce.service.AuthService;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -21,18 +20,15 @@ public class AuthController {
 
     private final AuthService service;
 
-    @CrossOrigin(origins = "http://localhost:5173")
     @PostMapping("/register")
     public AuthenticationResponseDTO registerUser(
             @Valid @RequestBody RegisterRequestDTO request) {
         return service.registerUser(request);
     }
 
-    @CrossOrigin(origins = "http://localhost:5173")
     @PostMapping("/authenticate")
     public AuthenticationResponseDTO authenticateUser(
             @Valid @RequestBody AuthenticationRequestDTO request) {
-        System.out.println(request);
         return service.authenticateUser(request);
     }
 
