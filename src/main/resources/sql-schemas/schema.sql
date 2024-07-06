e9172511dc6fa3fd2921a68de011f5351c4a06ed
gregoriocarranza <gregoriocarranza@hotmail.com>
2024-07-06 15:33:43 -0300

    schemaChange

--- src/main/resources/sql-schemas/schema.sql
+++ src/main/resources/sql-schemas/schema.sql
@@ -4,6 +4,23 @@ CREATE DATABASE `ecommerce-app`;
 
 USE `ecommerce-app`;
 
+-- Creacion de la tabla users
+CREATE TABLE `users` (
+    `id` BIGINT AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(255) NOT NULL,
+    `last_name` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `role` VARCHAR(255) DEFAULT 'COMPRADOR' CHECK (role IN ('VISITANTE', 'VENDEDOR', 'COMPRADOR'))
+);
+
+-- Creacion de la tabla discounts
+CREATE TABLE `discounts` (
+    `id` BIGINT AUTO_INCREMENT PRIMARY KEY,
+    `code` VARCHAR(255) NOT NULL,
+    `percentage` DECIMAL(10, 2) NOT NULL,
+    `expiry_date` DATE NOT NULL
+);
 -- Creacion de la tabla products
 CREATE TABLE `products` (
     `id` BIGINT AUTO_INCREMENT PRIMARY KEY,
@@ -12,6 +29,8 @@ CREATE TABLE `products` (
     `url_image` VARCHAR(255),
     `stock` INT,
     `description` TEXT,
+    `active` BOOLEAN DEFAULT TRUE,
+    `seller_id` BIGINT,
     `category` VARCHAR(255) CHECK (
         `category` IN (
             'FOOTWEAR',
@@ -35,19 +54,8 @@ CREATE TABLE `products` (
             'TROUSERS',
             'BALLS'
         )
-    )
-);
-
--- Creacion de la tabla users
-CREATE TABLE `users` (
-    `id` BIGINT AUTO_INCREMENT PRIMARY KEY,
-    `name` VARCHAR(255) NOT NULL,
-    `last_name` VARCHAR(255) NOT NULL,
-    `email` VARCHAR(255) NOT NULL,
-    `password` VARCHAR(255) NOT NULL,
-    `role` VARCHAR(255) DEFAULT 'COMPRADOR' CHECK (role IN ('VISITANTE', 'VENDEDOR', 'COMPRADOR'))
-
-
+    ),
+    CONSTRAINT `fk_seller_id` FOREIGN KEY (`seller_id`) REFERENCES `users` (`id`)
 );
 
 -- Creacion de la tabla buys
@@ -56,39 +64,18 @@ CREATE TABLE `buys` (
     `total` DECIMAL(19, 2) NOT NULL,
     `status` VARCHAR(255),
     `buyer_id` BIGINT NOT NULL,
-    FOREIGN KEY (`buyer_id`) REFERENCES `users` (`id`)
+    `discount_id` BIGINT,
+    CONSTRAINT `fk_buys_buyer_id` FOREIGN KEY (`buyer_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+    CONSTRAINT `fk_discount_id` FOREIGN KEY (`discount_id`) REFERENCES `discounts` (`id`)
 );
 
 -- Creacion de la tabla item_products
 CREATE TABLE `item_products` (
     `id` BIGINT AUTO_INCREMENT PRIMARY KEY,
     `buy_number` BIGINT NOT NULL,
-    `product_id` BIGINT NOT NULL,
+    `product_id` BIGINT ,
     `quantity` INT NOT NULL,
-    FOREIGN KEY (`buy_number`) REFERENCES `buys` (`number`),
-    FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
+    CONSTRAINT `fk_item_products_buy_number` FOREIGN KEY (`buy_number`) REFERENCES `buys` (`number`) ON DELETE CASCADE,
+    CONSTRAINT `fk_item_products_product_id` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE SET NULL
 );
 
--- Creacion de la tabla discounts
-CREATE TABLE `discounts` (
-    `id` BIGINT AUTO_INCREMENT PRIMARY KEY,
-    `code` VARCHAR(255) NOT NULL,
-    `amount` DOUBLE NOT NULL,
-    `expiry_date` DATE NOT NULL
-);
-
-ALTER TABLE `buys` ADD COLUMN `discount_id` BIGINT;
-
-ALTER TABLE `buys`
-ADD CONSTRAINT `fk_discount_id` FOREIGN KEY (`discount_id`) REFERENCES `discounts` (`id`);
-
-ALTER TABLE `discounts`
-MODIFY COLUMN `amount` DECIMAL(10, 2) NOT NULL;
-
-ALTER TABLE `products` ADD COLUMN `seller_id` BIGINT;
-
-ALTER TABLE `products`
-ADD CONSTRAINT `fk_seller_id` FOREIGN KEY (`seller_id`) REFERENCES `users` (`id`);
-
-ALTER TABLE `discounts`
-RENAME COLUMN `amount` TO `percentage`;
